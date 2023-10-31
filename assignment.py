facfc8eb862cf0ef4dbdffaab00128c09a79f267
Minjik06 <minjik525@deltalearns.ca>
2023-10-31 12:58:54 -0700

    sdf

--- assignment.py
+++ assignment.py
@@ -37,9 +37,9 @@ create table if not exists pets (
     pspecies tinytext,
     pbreed tinytext,
     oname tinytext,
-    ophone int,
-    obalance int,
-    date Char(10));
+    ophone tinytext,
+    obalance tinytext,
+    date tinytext);
 '''
 cursor.execute(query)
 cursor.execute('PRAGMA table_info(pets);')
@@ -49,3 +49,34 @@ print(result)
 for i in result:
     print(i)
 
+data = [
+    [1789,'Bella','Dog','Maltese','Joe Mantenga', '778457120', '700','2022-01-28'],
+    [985,'Lucy','Dog','Bichon','Hanna Montana', '7784857629', '950','2020-11-13'],
+    [2157,'Chloe','Cat','persian','Amanda Huggenkis', '7784475012', '630','2023-03-21'],
+    [2845,'Max','Dog','poodle','Michael Jackson', '7789851068', '810','2023-10-28'],
+    [1318,'Coco','Bird','parrot','Peter Nesmith', '7785736489', '370','2021-05-03']
+    ]
+
+for i in data:
+    query = f"insert into pets (id,pname,pspecies,pbreed,oname,ophone,obalance,date) values ({i[0]},'{i[1]}','{i[2]}','{i[3]}','{i[4]}','{i[5]}','{i[6]}','{i[7]}');"
+    print(query)
+    cursor.execute(query)
+connection.commit()
+query = "select * from pets"
+cursor.execute(query)
+result = cursor.fetchall()
+print(result)
+for i in result:
+    print(i)
+
+
+
+"""create table if not exists pets (
+    id integer primary key autoincrement,
+    pname tinytext,
+    pspecies tinytext,
+    pbreed tinytext,
+    oname tinytext,
+    ophone int,
+    obalance integer,
+    date Char(10));"""
