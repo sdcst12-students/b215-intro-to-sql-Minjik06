34e6797fa9e3b888c2a26f264d62f71943a0a526
Minjik06 <minjik525@deltalearns.ca>
2023-10-31 13:18:47 -0700

    sd

--- assignment.py
+++ assignment.py
@@ -41,42 +41,36 @@ create table if not exists pets (
     obalance tinytext,
     date tinytext);
 '''
+
+"""
 cursor.execute(query)
 cursor.execute('PRAGMA table_info(pets);')
 
 result = cursor.fetchall()
 print(result)
 for i in result:
-    print(i)
+    print(i)"""
 
+cursor.execute('delete from pets') 
 data = [
-    [1789,'Bella','Dog','Maltese','Joe Mantenga', '778457120', '700','2022-01-28'],
-    [985,'Lucy','Dog','Bichon','Hanna Montana', '7784857629', '950','2020-11-13'],
-    [2157,'Chloe','Cat','persian','Amanda Huggenkis', '7784475012', '630','2023-03-21'],
+    [1789,'Bella','Dog','Maltese','Joe Mantenga', '778457120', '700','2020-01-28'],
+    [985,'Lucy','Dog','Bichon','Hanna Montana', '7784857629', '950','2021-11-13'],
+    [2157,'Chloe','Cat','persian','Amanda Huggenkis', '7784475012', '630','2022-03-21'],
     [2845,'Max','Dog','poodle','Michael Jackson', '7789851068', '810','2023-10-28'],
-    [1318,'Coco','Bird','parrot','Peter Nesmith', '7785736489', '370','2021-05-03']
+    [1318,'Coco','Bird','parrot','Peter Nesmith', '7785736489', '370','2023-05-03']
     ]
 
 for i in data:
-    query = f"insert into pets (id,pname,pspecies,pbreed,oname,ophone,obalance,date) values ({i[0]},'{i[1]}','{i[2]}','{i[3]}','{i[4]}','{i[5]}','{i[6]}','{i[7]}');"
+    query = f"insert into pets (pname,pspecies,pbreed,oname,ophone,obalance,date) values ('{i[1]}','{i[2]}','{i[3]}','{i[4]}','{i[5]}','{i[6]}','{i[7]}');"
     print(query)
     cursor.execute(query)
 connection.commit()
 query = "select * from pets"
 cursor.execute(query)
+
+
 result = cursor.fetchall()
 print(result)
 for i in result:
     print(i)
 
-
-
-"""create table if not exists pets (
-    id integer primary key autoincrement,
-    pname tinytext,
-    pspecies tinytext,
-    pbreed tinytext,
-    oname tinytext,
-    ophone int,
-    obalance integer,
-    date Char(10));"""
